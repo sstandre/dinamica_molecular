7c58a746945eb2c4cf0d3b3e9eaf59f19b80add1
sstandre <simon.saint.a@gmail.com>
2021-11-25 15:39:23 -0300

    eliminar subexpresiones comunes en force y verlet. no se si es mas rapido

--- verlet.f90
+++ verlet.f90
@@ -1,18 +1,22 @@
 subroutine verlet
     use globals
     implicit none
+    real(8) :: temp1, temp2
+
+    temp1 = dt**2/(2*m)
+    temp2 = dt/(2*m)
 
     ! Actualizo posiciones a t+dt
-    r = r + v*dt +f*(dt**2/(2*m))
+    r = r + v*dt +f*temp1
     ! Condiciones de contorno periodicas para la posicion
     r = modulo(r, L)
     ! Velocidad "intermedia" (en t+dt/2)
-    v = v + f * (dt/(2*m))
+    v = v + f * temp2
     ! Actualizo el potencial y las fuerzas a t+dt
     call force(1)
-    ! Actualizar las fuerzas con el termostato de Langevin (!!!!, tengo v intermedia!)
+    ! Actualizar las fuerzas con el termostato de Langevin
     call lang()
     ! Velocidad "final" (en t+dt)
-    v = v + f *(dt/(2*m))
+    v = v + f * temp2
 
 end subroutine
